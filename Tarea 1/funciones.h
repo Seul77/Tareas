7af45a2f17d29afae226c9f97bfd4a8fc9b744b3
Seul77 <a01714080@tec.mx>
2026-08-27 22:12:33 -0600

    Add files via upload

--- Tarea 1/funciones.h
+++ Tarea 1/funciones.h
@@ -1,1 +1,29 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
 
+#include <iostream>
+using namespace std;
+
+class Funciones{
+    public: 
+    int sumaIterativa(int n){
+        int a = 0;
+        for (int i=1; i<=n; i++){
+            a = a + i;
+    }
+    return a;
+    };
+
+    int sumaRecursiva(int n){
+        if (n==0){
+            return 0;
+        }
+        return n + sumaRecursiva(n-1);
+    } 
+
+    int sumaDirecta(int n){
+        return (n*(n+1)/2);
+    }
+};
+
+#endif
